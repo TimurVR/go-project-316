1eccc5c1728d06c48cc77d83d2ae2850380f777a
TimurVR <138249026+TimurVR@users.noreply.github.com>
2026-03-02 13:40:04 +0300

    The ninth commit

--- crawler/analyze.go
+++ crawler/analyze.go
@@ -147,9 +147,6 @@ func Analyze(ctx context.Context, opts Options) ([]byte, error) {
 		GeneratedAt: time.Now().UTC(),
 		Pages:       make([]Page, 0),
 	}
-	if opts.Depth == 1 {
-		return json.Marshal(report)
-	}
 	visited := make(map[string]bool)
 	visitedMu := sync.Mutex{}
 	taskChan := make(chan CrawlTask, 2000)
