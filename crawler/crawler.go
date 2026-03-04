d85f4db1f429d3f898cb99a34698d638b544843b
TimurVR <138249026+TimurVR@users.noreply.github.com>
2026-03-04 18:13:22 +0300

    The ninth commit

--- crawler/crawler.go
+++ crawler/crawler.go
@@ -68,7 +68,9 @@ func Analyze(ctx context.Context, opts Options) ([]byte, error) {
 	if opts.Concurrency <= 0 {
 		opts.Concurrency = 1
 	}
-
+	if opts.Depth < 0 {
+		opts.Depth = 0
+	}
 	rawRoot, _ := NormalizeURL(opts.URL, nil)
 	rootURL, _ := url.Parse(rawRoot)
 
